77c595135778554f08569a9ca994aecf76b64d46
nanxmas <lmc26817@outlook.com>
2024-04-28 19:52:03 +0800

    uptime

---
+++ uptime/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '查询小难状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2535041-31a7ad5059daf8eedc416a45',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Chat',
+      url: 'https://https://bing.xinu.ink/'
+    },
+    {
+      text: '天气☁️',
+      url: 'https://nanxmas.github.io/Weather'
+    },
+    {
+      text: '100％晴天女孩',
+      url: 'https://nanxmas.github.io/Otenki/'
+    },
+  ],
+};
